e4e1b7201c12714a9804f817c26121b6babd037e
yangjiwoong <wpyang123@naver.com>
2023-06-03 00:09:13 +0900

    add item functions

--- settings.cpp
+++ settings.cpp
@@ -20,6 +20,8 @@ void init_pair_colors(){
     init_pair(1, COLOR_BLACK, COLOR_BLACK); // filed
     init_pair(2, COLOR_WHITE, COLOR_WHITE); // immune wall
     init_pair(3, COLOR_GREEN, COLOR_GREEN); // wall
+    init_pair(5, COLOR_BLUE, COLOR_BLUE); // Growth
+    init_pair(6, COLOR_MAGENTA, COLOR_MAGENTA); // Poison
     init_pair(11, COLOR_YELLOW, COLOR_YELLOW); // head
     init_pair(12, COLOR_CYAN, COLOR_CYAN); // tail
 }
