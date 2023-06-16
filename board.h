c35f2d4f7f83da746fe9a685bce6a799b0ad8d30
jjunh33 <jjunh33@naver.com>
2023-06-16 14:57:57 +0900

    :sparkles: 시간에 따른 점수 추가

--- board.h
+++ board.h
@@ -10,20 +10,22 @@ using namespace std;
 
 class Board
 {
-int curB; // Current Length
-int maxB; // Max Length
-int countPoison; // 독 아이템 먹은 횟수
-int countGrowth; // 성장 아이템 먹은 횟수
-int countGate; // 게이트 이용한 횟수
+    int curB;        // Current Length
+    int maxB;        // Max Length
+    int countPoison; // 독 아이템 먹은 횟수
+    int countGrowth; // 성장 아이템 먹은 횟수
+    int countGate;   // 게이트 이용한 횟수
+    int countSec;
 
 public:
     void init();
-    WINDOW* makeMissionBoard();
-    WINDOW* makeScoreBoard(); 
-    void countPlus(); // 성장 아이템을 성장 아이템 먹은 횟수가 늘어남
-    void countMinus(); // 독 아이템을 먹으면 독 아이템 먹은 횟수가 늘어남
-    void countUsedGate(); // 게이트를 사용하면 게이트 이용한 횟수가 늘어남
-    void count_init(Snake &snake); // 상태 초기화
+    WINDOW *makeMissionBoard();
+    WINDOW *makeScoreBoard();
+    void countPlus();                // 성장 아이템을 성장 아이템 먹은 횟수가 늘어남
+    void countMinus();               // 독 아이템을 먹으면 독 아이템 먹은 횟수가 늘어남
+    void countUsedGate();            // 게이트를 사용하면 게이트 이용한 횟수가 늘어남
+    void countSecPlus();             // 1초마다 시간점수가 늘어남
+    void count_init(Snake &snake);   // 상태 초기화
     void update_score(Snake &snake); // 아이템을 먹으면 갱신
     void nextStage();
 };
